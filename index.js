95d3b8eac2867f1edcbd74d267046618ae8355a9
Kate <115913025+bootonk@users.noreply.github.com>
2023-05-23 22:00:14 -0700

    Route and API call for weather results

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
-
 const express = require("express");
-const bodyParser = require("body-parser");
 const axios = require("axios");
+const bodyParser = require("body-parser");
 
 const PORT = 8080;
 const app = express();
 
+app.set("view engine", "ejs");
+
+const fetchWeather = function (callback) {
+  const api_key = process.env.WEATHER_API;
+  const weather_url = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=V9A5C4&aqi=no`;
+
+  axios.get(weather_url).then((response) => {
+    return console.log(response);
+  });
+};
+
+fetchWeather();
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("weather");
 });
 
 app.listen(PORT, () => {
