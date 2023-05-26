d86feaaf4a4433244441ae3696f17e2deffb7ab7
Kate <115913025+bootonk@users.noreply.github.com>
2023-05-25 17:28:58 -0700

    Fix canvas code to show sketch within browser

--- public/sketch.js
+++ public/sketch.js
@@ -1,24 +1,12 @@
-function ready(fn) {
-  if (document.readyState !== "loading") {
-    fn();
-  } else {
-    document.addEventListener("DOMContentLoaded", fn);
-
-    function setup() {
-      createCanvas(400, 400);
-    }
-
-    function draw() {
-      if (mouseIsPressed) {
-        fill(0);
-      } else {
-        fill(255);
-      }
-      ellipse(mouseX, mouseY, 80, 80);
-    }
-  }
+function setup() {
+  createCanvas(400, 400);
 }
 
-ready(Alpine.start);
-setup();
-draw();
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
